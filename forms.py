29d2cefb729c54c6db9737f601edfd9744d234b4
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-26 10:51:09 +0300

    flask forms

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, BooleanField, RadioField
+from wtforms.validators import InputRequired, Length
+
+
+class CourseForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    enrolment_num = StringField('GIC Enrolment Number', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    course_rating = RadioField('Pick One',
+                               choices=['Very Well', 'Well', 'Average', 'Poorly', 'Very Poorly'],
+                               validators=[InputRequired()])
