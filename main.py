d1f9cb5aef18716243b48e62a63454f7c19bddbf
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-26 16:30:54 +0300

    flask forms

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from forms import CourseForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ab8aec9165e8ba37f041564c'
@@ -8,6 +9,18 @@ app.config['SECRET_KEY'] = 'ab8aec9165e8ba37f041564c'
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = CourseForm()
+    #name = request.form['name']
+    # enrolment_num = request.form['enrolment_num']
+    # email = request.form['email']
+    # course_rating = request.form['course_review']
+    # course_review = request.form['course_rating']
+    # if request.method == 'POST':
+    #     with open('course_output', 'w') as text:
+    #         text.write(str(name))
+    #         text.write(str(enrolment_num))
+    #         text.write(str(email))
+    #         text.write(str(course_rating))
+    #         text.write(str(course_review))
     return render_template('index.html', form=form)
 
 @app.route('/success', methods=['GET', 'POST'])
@@ -15,4 +28,4 @@ def success():
     return render_template('success.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
